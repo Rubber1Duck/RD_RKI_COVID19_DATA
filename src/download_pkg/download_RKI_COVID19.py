0da5c4c8ef0696aaf0b608fc44cc0c3e1181db6f
Peter Markewitz <peter@markewitz.org>
2022-04-21 19:33:31 +0000

    compress RKI File

--- src/download_pkg/download_RKI_COVID19.py
+++ src/download_pkg/download_RKI_COVID19.py
@@ -9,5 +9,5 @@ def download_RKI_COVID19():
     filename = "RKI_COVID19.csv"
     url = "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
 
-    a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=False, add_date=True, add_latest=False)
+    a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=True, add_date=True, add_latest=False)
     a.write_file()

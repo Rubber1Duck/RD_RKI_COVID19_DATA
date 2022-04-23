5e379cdcaa3b9226f8a30ac1723bdcc7895b5b2b
Peter Markewitz <peter@markewitz.org>
2022-04-23 10:37:15 +0000

    rename csv files, add BL files, limit to lats 14 days

--- src/process_RKI_Covid_initial.py
+++ src/process_RKI_Covid_initial.py
@@ -24,10 +24,13 @@ dtypes_new_2 = {'IdBundesland': 'Int32', 'IdLandkreis': 'Int32', 'NeuerFall': 'I
 dtypes_old = {'IdBundesland': 'Int32', 'Landkreis ID': 'Int32', 'Neuer Fall': 'Int8',
               'Neuer Todesfall': 'Int8', 'AnzahlFall': 'Int32', 'AnzahlTodesfall': 'Int32', 'Meldedatum': 'object',
               'Datenstand': 'object'}
-dtypes_fallzahlen = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'IdLandkreis': 'Int32',
+dtypes_fallzahlen_LK = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'IdLandkreis': 'Int32',
                      'AnzahlFall': 'Int32', 'AnzahlTodesfall': 'Int32', 'AnzahlFall_neu': 'Int32',
                      'AnzahlTodesfall_neu': 'Int32', 'AnzahlFall_7d': 'Int32', 'report_date': 'object',
                      'meldedatum_max': 'object'}
+dtypes_fallzahlen_BL = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'AnzahlFall': 'Int32',
+                     'AnzahlTodesfall': 'Int32', 'AnzahlFall_neu': 'Int32', 'AnzahlTodesfall_neu': 'Int32',
+                     'AnzahlFall_7d': 'Int32', 'report_date': 'object', 'meldedatum_max': 'object'}
 # %%
 all_files = []
 for file in file_list:
@@ -44,60 +47,76 @@ for file_path_full, report_date in all_files:
     if report_date >= date(2020, 3, 24):
         t1 = process_time()
         print(report_date)
-        path_date_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen',
-                               'RKI_COVID19_Fallzahlen_' + report_date.strftime("%Y%m%d") + '.csv')
+        path_date_csv_LK = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen',
+                               'FixFallzahlen_' + report_date.strftime("%Y%m%d") + '_LK.csv')
+        path_date_csv_BL = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen',
+                               'FixFallzahlen_' + report_date.strftime("%Y%m%d") + '_BL.csv')
         if report_date == date(2020, 3, 25):
             # Sonderfall, falscher Datentyp in Zeile
-            df = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new, skiprows=[10572])
+            df_LK = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new, skiprows=[10572])
         elif report_date == date(2020, 3, 26):
             # Sonderfall, falscher Datentyp in Zeile
-            df = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new, skiprows=[1001])
+            df_LK = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new, skiprows=[1001])
         else:
             try:
-                df = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new)
+                df_LK = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new)
             except UnicodeDecodeError:
                 try:
-                    df = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new,
+                    df_LK = pd.read_csv(file_path_full, usecols=dtypes_new.keys(), dtype=dtypes_new,
                                      encoding='cp1252')
                 except ValueError:
-                    df = pd.read_csv(file_path_full, usecols=dtypes_new_2.keys(), dtype=dtypes_new_2,
+                    df_LK = pd.read_csv(file_path_full, usecols=dtypes_new_2.keys(), dtype=dtypes_new_2,
                                      encoding='cp1252')
             except ValueError:
-                df = pd.read_csv(file_path_full, usecols=dtypes_old.keys(), dtype=dtypes_old)
+                df_LK = pd.read_csv(file_path_full, usecols=dtypes_old.keys(), dtype=dtypes_old)
 
-            df.rename(columns={'Neuer Fall': 'NeuerFall', 'Neuer Todesfall': 'NeuerTodesfall',
+            df_LK.rename(columns={'Neuer Fall': 'NeuerFall', 'Neuer Todesfall': 'NeuerTodesfall',
                                'Landkreis ID': 'IdLandkreis', 'Meldedatum2': 'Meldedatum'}, inplace=True)
         try:
-            df['Meldedatum'] = pd.to_datetime(df['Meldedatum']).dt.date
+            df_LK['Meldedatum'] = pd.to_datetime(df_LK['Meldedatum']).dt.date
         except:
-            df['Meldedatum'] = pd.to_datetime(df['Meldedatum'], unit='ms').dt.date
+            df_LK['Meldedatum'] = pd.to_datetime(df_LK['Meldedatum'], unit='ms').dt.date
         try:
-            datenstand = pd.to_datetime(df['Datenstand'].iloc[0])
+            datenstand = pd.to_datetime(df_LK['Datenstand'].iloc[0])
         except:
-            datenstand = pd.to_datetime(df['Datenstand'].iloc[0], format='%d.%m.%Y, %H:%M Uhr')
-        df['AnzahlFall_neu'] = np.where(df['NeuerFall'].isin([-1, 1]), df['AnzahlFall'], 0)
-        df['AnzahlFall'] = np.where(df['NeuerFall'].isin([0, 1]), df['AnzahlFall'], 0)
-        df['AnzahlFall_7d'] = np.where(df['Meldedatum'] > (datenstand - timedelta(days=8)),
-                                       df['AnzahlFall'], 0)
-        df['AnzahlTodesfall_neu'] = np.where(df['NeuerTodesfall'].isin([-1, 1]), df['AnzahlTodesfall'], 0)
-        df['AnzahlTodesfall'] = np.where(df['NeuerTodesfall'].isin([0, 1]), df['AnzahlTodesfall'], 0)
-        df.drop(['NeuerFall', 'NeuerTodesfall', 'Datenstand'], inplace=True, axis=1)
+            datenstand = pd.to_datetime(df_LK['Datenstand'].iloc[0], format='%d.%m.%Y, %H:%M Uhr')
+        df_LK['AnzahlFall_neu'] = np.where(df_LK['NeuerFall'].isin([-1, 1]), df_LK['AnzahlFall'], 0)
+        df_LK['AnzahlFall'] = np.where(df_LK['NeuerFall'].isin([0, 1]), df_LK['AnzahlFall'], 0)
+        df_LK['AnzahlFall_7d'] = np.where(df_LK['Meldedatum'] > (datenstand - timedelta(days=8)),
+                                       df_LK['AnzahlFall'], 0)
+        df_LK['AnzahlTodesfall_neu'] = np.where(df_LK['NeuerTodesfall'].isin([-1, 1]), df_LK['AnzahlTodesfall'], 0)
+        df_LK['AnzahlTodesfall'] = np.where(df_LK['NeuerTodesfall'].isin([0, 1]), df_LK['AnzahlTodesfall'], 0)
+        df_LK.drop(['NeuerFall', 'NeuerTodesfall', 'Datenstand'], inplace=True, axis=1)
+        df_BL = df_LK.copy()
+        df_BL['IdLandkreis'] = 0
+        df_GER = df_BL.copy()
+        df_GER['IdBundesland'] = 0
         agg_key = {}
-        for c in df.columns:
+        for c in df_LK.columns:
             if c not in key_list:
                 if c in ['Meldedatum']:
                     agg_key[c] = 'max'
                 else:
                     agg_key[c] = 'sum'
-        df = df.groupby(key_list, as_index=False).agg(agg_key)
-        df['report_date'] = report_date
-        df.rename(columns={'Meldedatum': 'meldedatum_max'}, inplace=True)
-        df['Datenstand'] = datenstand
-        covid_df = df
-        covid_df.sort_values(by=['report_date', 'IdLandkreis'], inplace=True)
-        with open(path_date_csv, 'wb') as csvfile:
-            covid_df.to_csv(csvfile, index=False, header=True, line_terminator='\n', encoding='utf-8', date_format='%Y-%m-%d',
-                    columns=dtypes_fallzahlen.keys())
+        df_LK = df_LK.groupby(key_list, as_index=False).agg(agg_key)
+        df_BL = df_BL.groupby(key_list, as_index=False).agg(agg_key)
+        df_GER = df_GER.groupby(key_list, as_index=False).agg(agg_key)
+        df_BL = pd.concat([df_GER, df_BL])
+        df_BL.drop(['IdLandkreis'], inplace=True, axis=1)
+        df_LK['report_date'] = report_date
+        df_BL['report_date'] = report_date
+        df_LK.rename(columns={'Meldedatum': 'meldedatum_max'}, inplace=True)
+        df_BL.rename(columns={'Meldedatum': 'meldedatum_max'}, inplace=True)
+        df_LK['Datenstand'] = datenstand
+        df_BL['Datenstand'] = datenstand
+        df_LK.sort_values(by=['report_date', 'IdBundesland', 'IdLandkreis'], inplace=True)
+        df_BL.sort_values(by=['report_date', 'IdBundesland'], inplace=True)
+        with open(path_date_csv_LK, 'wb') as csvfile:
+            df_LK.to_csv(csvfile, index=False, header=True, line_terminator='\n', encoding='utf-8', date_format='%Y-%m-%d',
+                    columns=dtypes_fallzahlen_LK.keys())
+        with open(path_date_csv_BL, 'wb') as csvfile:
+            df_BL.to_csv(csvfile, index=False, header=True, line_terminator='\n', encoding='utf-8', date_format='%Y-%m-%d',
+                    columns=dtypes_fallzahlen_BL.keys())
         t2 = process_time()
         t = t2 - t1
         print('Rechenzeit: ', t)
--- src/process_RKI_Covid_update.py
+++ src/process_RKI_Covid_update.py
@@ -1,5 +1,6 @@
 import os
-from datetime import timedelta, datetime
+import re
+from datetime import *
 
 import numpy as np
 import pandas as pd
@@ -16,44 +17,91 @@ path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
 
 iso_date_re = '([0-9]{4})(-?)(1[0-2]|0[1-9])\\2(3[01]|0[1-9]|[12][0-9])'
 pattern = 'RKI_COVID19'
-dtypes_fallzahlen = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'IdLandkreis': 'Int32',
+dtypes_fallzahlen_LK = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'IdLandkreis': 'Int32',
                      'AnzahlFall': 'Int32', 'AnzahlTodesfall': 'Int32', 'AnzahlFall_neu': 'Int32',
                      'AnzahlTodesfall_neu': 'Int32','AnzahlFall_7d': 'Int32', 'report_date': 'object',
                      'meldedatum_max': 'object'}
+dtypes_fallzahlen_BL = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'AnzahlFall': 'Int32',
+                     'AnzahlTodesfall': 'Int32', 'AnzahlFall_neu': 'Int32', 'AnzahlTodesfall_neu': 'Int32',
+                     'AnzahlFall_7d': 'Int32', 'report_date': 'object', 'meldedatum_max': 'object'}
 dtypes_covid = {'Datenstand': 'object', 'IdBundesland': 'Int32', 'IdLandkreis': 'Int32', 'NeuerFall': 'Int8',
                 'NeuerTodesfall': 'Int8', 'AnzahlFall': 'Int32', 'AnzahlTodesfall': 'Int32', 'Meldedatum': 'object'}
-key_list = ['Datenstand', 'IdBundesland', 'IdLandkreis']
+key_list_LK = ['Datenstand', 'IdBundesland', 'IdLandkreis']
+key_list_BL = ['Datenstand', 'IdBundesland']
 
 
 # %% read covid latest
 covid_path_latest, date_latest = find_latest_file(os.path.join(path), file_pattern=pattern)
-covid_df = pd.read_csv(covid_path_latest, usecols=dtypes_covid.keys(), dtype=dtypes_covid)
+covid_df_LK = pd.read_csv(covid_path_latest, usecols=dtypes_covid.keys(), dtype=dtypes_covid)
 
 # %% eval fallzahlen new
 print(date_latest)
-covid_df['Meldedatum'] = pd.to_datetime(covid_df['Meldedatum']).dt.date
-meldedatum_max = covid_df['Meldedatum'].max()
-covid_df['AnzahlFall_neu'] = np.where(covid_df['NeuerFall'].isin([-1, 1]), covid_df['AnzahlFall'], 0)
-covid_df['AnzahlFall'] = np.where(covid_df['NeuerFall'].isin([0, 1]), covid_df['AnzahlFall'], 0)
-covid_df['AnzahlFall_7d'] = np.where(covid_df['Meldedatum'] > (meldedatum_max - timedelta(days=7)),
-                                     covid_df['AnzahlFall'], 0)
-covid_df['AnzahlTodesfall_neu'] = np.where(covid_df['NeuerTodesfall'].isin([-1, 1]), covid_df['AnzahlTodesfall'], 0)
-covid_df['AnzahlTodesfall'] = np.where(covid_df['NeuerTodesfall'].isin([0, 1]), covid_df['AnzahlTodesfall'], 0)
-datenstand = pd.to_datetime(covid_df['Datenstand'].iloc[0], format='%d.%m.%Y, %H:%M Uhr')
-covid_df['Datenstand'] = datenstand.date()
-covid_df.drop(['NeuerFall', 'NeuerTodesfall'], inplace=True, axis=1)
+covid_df_LK['Meldedatum'] = pd.to_datetime(covid_df_LK['Meldedatum']).dt.date
+meldedatum_max = covid_df_LK['Meldedatum'].max()
+covid_df_LK['AnzahlFall_neu'] = np.where(covid_df_LK['NeuerFall'].isin([-1, 1]), covid_df_LK['AnzahlFall'], 0)
+covid_df_LK['AnzahlFall'] = np.where(covid_df_LK['NeuerFall'].isin([0, 1]), covid_df_LK['AnzahlFall'], 0)
+covid_df_LK['AnzahlFall_7d'] = np.where(covid_df_LK['Meldedatum'] > (meldedatum_max - timedelta(days=7)),
+                                     covid_df_LK['AnzahlFall'], 0)
+covid_df_LK['AnzahlTodesfall_neu'] = np.where(covid_df_LK['NeuerTodesfall'].isin([-1, 1]), covid_df_LK['AnzahlTodesfall'], 0)
+covid_df_LK['AnzahlTodesfall'] = np.where(covid_df_LK['NeuerTodesfall'].isin([0, 1]), covid_df_LK['AnzahlTodesfall'], 0)
+datenstand = pd.to_datetime(covid_df_LK['Datenstand'].iloc[0], format='%d.%m.%Y, %H:%M Uhr')
+covid_df_LK['Datenstand'] = datenstand.date()
+covid_df_LK.drop(['NeuerFall', 'NeuerTodesfall'], inplace=True, axis=1)
+covid_df_BL = covid_df_LK.copy()
+covid_df_BL['IdLandkreis'] = 0
+covid_df_GER = covid_df_BL.copy()
+covid_df_GER['IdBundesland'] = 0
 agg_key = {
     c: 'max' if c in ['Meldedatum', 'Datenstand'] else 'sum'
-    for c in covid_df.columns
-    if c not in key_list
+    for c in covid_df_LK.columns
+    if c not in key_list_LK
 }
+covid_df_LK = covid_df_LK.groupby(key_list_LK, as_index=False).agg(agg_key)
+covid_df_BL = covid_df_BL.groupby(key_list_LK, as_index=False).agg(agg_key)
+covid_df_GER = covid_df_GER.groupby(key_list_LK, as_index=False).agg(agg_key)
+covid_df_BL = pd.concat([covid_df_GER, covid_df_BL])
+covid_df_BL.drop(['IdLandkreis'], inplace=True, axis=1)
+covid_df_LK.rename(columns={'Meldedatum': 'meldedatum_max'}, inplace=True)
+covid_df_BL.rename(columns={'Meldedatum': 'meldedatum_max'}, inplace=True)
+covid_df_LK['report_date'] = date_latest
+covid_df_BL['report_date'] = date_latest
+path_date_csv_LK = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen',
+                               'FixFallzahlen_' + date_latest.strftime("%Y%m%d") + '_LK.csv')
+path_date_csv_BL = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen',
+                               'FixFallzahlen_' + date_latest.strftime("%Y%m%d") + '_BL.csv')
+covid_df_LK.sort_values(by=key_list_LK, inplace=True)
+covid_df_BL.sort_values(by=key_list_BL, inplace=True)
+with open(path_date_csv_LK, 'wb') as csvfile:
+    covid_df_LK.to_csv(csvfile, index=False, header=True, line_terminator='\n', encoding='utf-8',
+                          date_format='%Y-%m-%d', columns=dtypes_fallzahlen_LK.keys())
+with open(path_date_csv_BL, 'wb') as csvfile:
+    covid_df_BL.to_csv(csvfile, index=False, header=True, line_terminator='\n', encoding='utf-8',
+                          date_format='%Y-%m-%d', columns=dtypes_fallzahlen_BL.keys())
+# %% limit files to the last 14 days
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen')
 
-covid_df = covid_df.groupby(key_list, as_index=False).agg(agg_key)
-covid_df.rename(columns={'Meldedatum': 'meldedatum_max'}, inplace=True)
-covid_df['report_date'] = date_latest
-path_date_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Fallzahlen',
-                               'RKI_COVID19_Fallzahlen_' + date_latest.strftime("%Y%m%d") + '.csv')
-covid_df.sort_values(by=key_list, inplace=True)
-with open(path_date_csv, 'wb') as csvfile:
-    covid_df.to_csv(csvfile, index=False, header=True, line_terminator='\n', encoding='utf-8',
-                          date_format='%Y-%m-%d', columns=dtypes_fallzahlen.keys())
+iso_date_re = '([0-9]{4})(-?)(1[0-2]|0[1-9])\\2(3[01]|0[1-9]|[12][0-9])'
+file_list = os.listdir(path)
+file_list.sort(reverse=False)
+pattern = 'FixFallzahlen'
+
+all_files = []
+for file in file_list:
+    file_path_full = os.path.join(path, file)
+    if not os.path.isdir(file_path_full):
+        filename = os.path.basename(file)
+        re_filename = re.search(pattern, filename)
+        re_search = re.search(iso_date_re, filename)
+        if re_search and re_filename:
+            report_date = date(int(re_search.group(1)), int(re_search.group(3)), int(re_search.group(4)))
+            all_files.append((file_path_full, report_date))
+
+day_range = pd.date_range(end=datetime.today(), periods=14).tolist()
+day_range_str = []
+for datum in day_range:
+    day_range_str.append(datum.strftime('%Y%m%d'))
+for file_path_full, report_date in all_files:
+    if report_date.strftime('%Y%m%d') not in day_range_str:
+        os.remove(file_path_full)
+
+# %%

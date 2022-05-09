8693dd8ec75a7eec735c70232de6fe9fa395a771
Peter Markewitz <peter@markewitz.org>
2022-05-09 08:01:13 +0000

    change coding

--- src/download_pkg/download_RKI_COVID19.py
+++ src/download_pkg/download_RKI_COVID19.py
@@ -16,11 +16,16 @@ def download_RKI_COVID19():
 
     meta = DownloadFile(url=url_meta, filename=filename_meta, download_path=meta_path, compress=False, add_date=False, add_latest=False)
     meta.write_file()
-    with open(meta_path + "/" + filename_meta,'r') as file:
+    
+    with open(meta_path + "/" + filename_meta, 'r', encoding ='utf8') as file:
         metaobj = json.load(file)
+    
     metaobj['description'] = ""
     metaobj['licenseInfo'] = "Die Daten sind die Fallzahlen in Deutschland des Robert Koch-Institut (RKI) und stehen unter der Open Data Datenlizenz Deutschland – Namensnennung – Version 2.0 zur Verfügung."
-    json.dump(metaobj,open(meta_path + "/" + filename_meta,'w'))
+    metaobj['accessInformation'] = "Bundesamt für Kartographie und Geodäsie Robert Koch-Institut"
+    
+    with open(meta_path + "/" + filename_meta, 'w', encoding ='utf8') as json_file:
+        json.dump(metaobj, json_file, ensure_ascii = False)
 
     a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=True, add_date=True, add_latest=False)
     a.write_file()

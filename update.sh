0057b6978c543cc59dd4648379af8a4871971f26
Peter Markewitz <peter@markewitz.org>
2022-05-17 13:11:02 +0000

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+source $VIRTUAL_ENV/bin/activate
+cd /usr/src/app/src
+
 DATE=$(date '+%Y-%m-%d')
 
 URL_METADATA="https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74?f=json"
@@ -6,18 +10,17 @@ URL_METADATA="https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e4016
 modified=$(curl -s -X GET -H "Accept: application/json" "$URL_METADATA" 2>&1 | sed -E 's/.*"modified":([0-9]+)000.*/\1/')
 modified=$(date -d "@$modified" '+%Y-%m-%d')
 if [[ "$DATE" != "$modified" ]]; then
-  echo "Updated data for $date does not yet exist (modified date: $modified)"
+  echo "Updated data for $DATE does not yet exist (modified date: $modified)"
   exit 1
 fi
-modified=$(cat Fallzahlen/RKI_COVID19_meta.json 2>&1 | sed -E 's/.*"modified":([0-9]+)000.*/\1/')
+modified=$(cat ../Fallzahlen/RKI_COVID19_meta.json 2>&1 | sed -E 's/.*"modified": ([0-9]+)000.*/\1/')
 modified=$(date -d "@$modified" '+%Y-%m-%d')
 if [[ "$DATE" = "$modified" ]]; then
-  echo "modified data already downloaded for $date (modified date: $modified)"
+  echo "modified data already downloaded for $DATE (modified date: $modified)"
   exit 1
 fi
+
 # do the action
-source $VIRTUAL_ENV/bin/activate
-cd /usr/src/app/src
 python schedule.py
 python process_RKI_Covid_update.py
 python schedule_meta.py

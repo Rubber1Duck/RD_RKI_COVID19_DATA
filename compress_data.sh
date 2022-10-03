a6eab5609ade54c9bf3463392605341289837877
Peter Markewitz <peter@markewitz.org>
2022-10-03 20:30:37 +0000

    add check if compress_data.sh is running

--- compress_data.sh
+++ compress_data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# first check if the update script is running
+# first check if this script is already running
+if [ -f /tmp/compress.pid ]; then
+  DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
+  echo "$DATE2 : Compress script is already running."
+  exit 1
+fi
+
+# then check if the update script is running
 if [ -f /tmp/update.pid ]; then
   DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
   echo "$DATE2 : Update is still in progress! Will wait with compressing data."
@@ -15,8 +22,10 @@ if [ ! -f *.csv ]; then
   exit 1
 fi
 
+touch /tmp/compress.pid
 DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
 echo "$DATE : Start compressing csv files"
 /usr/bin/xz -zT0  *.csv
 DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
 echo "$DATE2 : All csv files are compressed"
+rm -rf /tmp/update.pid

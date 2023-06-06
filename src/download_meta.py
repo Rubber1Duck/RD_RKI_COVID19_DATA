31538e3634b0eb261e02fa1400b6a42e8930f807
Peter Markewitz <peter@markewitz.org>
2023-06-06 16:01:07 +0000

    right timestamp if actual data is used

--- src/download_meta.py
+++ src/download_meta.py
@@ -9,29 +9,37 @@ try:
     today = dt.date.today().strftime('%Y-%m-%d')
     url_meta = "https://github.com/robert-koch-institut/SARS-CoV-2-Infektionen_in_Deutschland/raw/main/Metadaten/zenodo.json"
     url_data = "https://github.com/robert-koch-institut/SARS-CoV-2-Infektionen_in_Deutschland/raw/main/Aktuell_Deutschland_SarsCov2_Infektionen.csv"
+    url_api = "https://api.github.com/repos/robert-koch-institut/SARS-CoV-2-Infektionen_in_Deutschland/commits/main"
+    
+    # get last commit json
+    api_resp = requests.get(url=url_api, allow_redirects=True)
+    api_Object = json.loads(api_resp.content.decode(encoding='utf8'))
+    
+    # get filesize
     filename = "Aktuell_Deutschland_SarsCov2_Infektionen.csv"
     resp = requests.head(url_data, allow_redirects=True)
     size = resp.headers['content-length']
 
-    meta = DownloadFile(url=url_meta, filename=filename_meta, download_path=meta_path)
-    meta.write_file()
-    
+    #get zenodo.json
+    meta_resp = requests.get(url=url_meta, allow_redirects=True)
+    metaObj = json.loads(meta_resp.content.decode(encoding='utf8'))
+        
+    # delete all what is not needed from metaObj
     needed =["publication_date", "version"]
-
-    with open(meta_path + "/" + filename_meta, 'r', encoding ='utf8') as file:
-        metaObj = json.load(file)
     newObj = metaObj.copy()
     for property in newObj:
         if property not in needed:
             del metaObj[property]
-    metaObj["size"] = size
-    metaObj["filename"] = filename
-    metaObj["url"] = url_data
-    pubDate = metaObj["publication_date"]
-    pubDateTz = dt.datetime.fromisoformat(pubDate)
+    # insert all needed stuff
+    metaObj["size"] = size # size of the file to download
+    metaObj["filename"] = filename # filename of the file to download
+    metaObj["url"] = url_data # url of the file to download
+    pubDate = api_Object['commit']['committer']['date'] # last commit date
+    pubDateTz = dt.datetime.strptime(pubDate, '%Y-%m-%dT%H:%M:%Sz') 
     pubDateTs = dt.datetime.timestamp(pubDateTz)
     modified = int(pubDateTs) * 1000
-    metaObj["modified"] = modified
+    metaObj["modified"] = modified # modified date timestamp
+    # store to disc 
     with open(meta_path + "/" + filename_meta, 'w', encoding ='utf8') as json_file:
         json.dump(metaObj, json_file, ensure_ascii = False)
 except Exception as e:

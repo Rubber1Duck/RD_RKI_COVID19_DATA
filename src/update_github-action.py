e389ee19c0a14dd058f2ad88c30fb0abceedf554
Peter Markewitz <peter@markewitz.org>
2023-07-29 04:33:47 +0000

    cleanup

--- src/update_github-action.py
+++ src/update_github-action.py
@@ -1,6 +1,5 @@
 import os
 import re
-#import requests as r
 import datetime as dt
 import numpy as np
 import pandas as pd
--- src/utils.py
+++ src/utils.py
@@ -18,36 +18,6 @@ def squeeze_dataframe(df):
 
     return df
 
-def panda_mem_usage(df, detail='full'):
-
-    dtypes = df.dtypes.reset_index(drop=False)
-    memory = df.memory_usage(deep=True).reset_index(drop=False)
-
-    df1 = pd.merge(dtypes, memory, on='index')
-    df1 = df1.rename(columns = {'index': 'col', '0_x': 'type', '0_y': 'bytes'})
-    total = df1['bytes'].sum()
-
-    objects = df.select_dtypes(include=['object', 'category'])
-    df_objs = objects.select_dtypes(include=['object', 'category']).describe().T.reset_index()
-
-    if detail == 'full':
-        print('')
-        print('{:<15} {:<15} {:>15} {:>8} {:>8}'.format('Column', 'Data Type', 'Bytes', 'MBs', 'GBs'))
-        print('{} {} {} {} {}'.format('-'*15, '-'*15, '-'*15, '-'*8, '-'*8))
-
-        for index, row in df1.iterrows():
-            print('{:<15} {:<15} {:>15,.0f} {:>8,.1f} {:>8,.2f}'.format(row['col'], str(row['type']), row['bytes'], row['bytes']/1024**2, row['bytes']/1024**3))
-
-        print('\nTotal: {:,.0f} Rows, {:,.0f} Bytes, {:,.1f} MBs, {:,.2f} GBs\n'.format(len(df), total, total/1024**2, total/1024**3))
-
-        print('{:<15} {:>13} {:>13}'.format('Column', 'Count', 'Unique'))
-        print('{} {} {}'.format('-'*15, '-'*13, '-'*13))
-        for index, row in df_objs.iterrows():
-            print('{:<15} {:>13,.0f} {:>13,.0f}'.format(row['index'], row['count'], row['unique']))
-
-    elif detail == 'return_short':
-        return len(df), total
-
 def write_file(df, fn, compression='', sheet_name='data'):
 
     fn_ext = os.path.splitext(fn)[1]

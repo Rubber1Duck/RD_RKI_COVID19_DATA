2f7c89566c40c787557153ecbf88c7ddd8889807
Peter Markewitz <peter@markewitz.org>
2023-08-12 04:55:48 +0000

    fill data

---
+++ recompress.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# download static 7zip
+DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
+echo "$DATE2 : download static 7zip"
+VERSION7ZIP="2301"
+./get7Zip.sh ${VERSION7ZIP}
+
+# start recompress all RKI_COVID19_*.csv.zx
+cd ./data
+for file in ./RKI_COVID19_*.csv.xz
+do
+  SIZE1=$(stat -c%s $file)
+  ../7zzs e -bso0 -bsp0 $file
+  rm -f $file
+  CSV="${file%.*}"
+  DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
+  echo "$DATE2 : start recompressing $file ($SIZE1 bytes)"
+  ../7zzs a -txz -mmt4 -mx=9 -sdel -stl -bso0 -bsp0 $file $CSV
+  SIZE2=$(stat -c%s $file)
+  QUOTE=$(gawk "BEGIN {OFMT=\"%.4f\"; print $SIZE2 / $SIZE1 * 100;}")
+  DATE2=$(date '+%Y-%m-%dT%H:%M:%SZ')
+  echo "$DATE2 : finished recompressing $file. New Size: $SIZE2 = $QUOTE %"
+done
+rm -rf ../7zzs

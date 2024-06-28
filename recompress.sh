e4e5aed92a244d9581fe2be0ca1f610de49a29e1
Peter Markewitz <peter@markewitz.org>
2024-06-28 15:31:09 +0000

    change recompress.sh

--- recompress.sh
+++ recompress.sh
@@ -10,20 +10,21 @@ PUSHLIST=""
 cd ./data
 for file in ./RKI_COVID19_*.csv.xz
 do
-  SIZE1=$(stat -c%s $file)
+  SIZEBEVOR=$(stat -c%s $file)
   ../7zzs e -bso0 -bsp0 $file
   rm -f $file
-  CSV="${file%.*}"
-  echo "$(date '+%Y-%m-%dT%H:%M:%SZ') : start recompressing $file ($SIZE1 bytes)"
+  CSVFILE="${file%.*}"
+  SIZECSV=$(stat -c%s $CSVFILE)
+  echo "$(date '+%Y-%m-%dT%H:%M:%SZ') : recompressing $CSVFILE $SIZECSV bytes (old compressed size was $SIZEBEVOR bytes)"
   ../7zzs a -txz -mmt4 -mx=9 -sdel -stl -bso0 -bsp0 $file $CSV
-  SIZE2=$(stat -c%s $file)
-  QUOTE=$(gawk "BEGIN {OFMT=\"%.4f\"; print $SIZE2 / $SIZE1 * 100;}")
-  if [[ $SIZE2 -lt $SIZE1 ]]; then
+  SIZEAFTER=$(stat -c%s $file)
+  QUOTE=$(gawk "BEGIN {OFMT=\"%.4f\"; print $SIZEAFTER / $SIZEBEVOR * 100;}")
+  if [[ $SIZEAFTER -nq $SIZEBEVOR ]]; then
     file=$(basename $file)
     PUSHLIST="./data/$file $PUSHLIST"
-    echo "$(date '+%Y-%m-%dT%H:%M:%SZ') : finished recompressing $file. New Size: $SIZE2 = $QUOTE %. Added to pushlist!"
+    echo "$(date '+%Y-%m-%dT%H:%M:%SZ') : done recompressing $file. New Size: $SIZEAFTER = $QUOTE %. Added to pushlist!"
   else
-    echo "$(date '+%Y-%m-%dT%H:%M:%SZ') : finished recompressing $file. New Size: $SIZE2 = $QUOTE %. Not added to pushlist!"
+    echo "$(date '+%Y-%m-%dT%H:%M:%SZ') : done recompressing $file. New Size: $SIZEAFTER = $QUOTE %. Not added to pushlist!"
   fi
 done
 rm -rf ../7zzs
